ddbacbc2b30b58f115aabd5b52e0e9a198ecf032
Ian Carlos Ortega <iancarlosortegaleon@gmail.com>
2023-03-27 17:01:43 -0500

    Home Page

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,30 @@ import { Navbar } from '../components';
 
 export default function Home() {
 	return (
-		<div>
-			<Navbar />
-		</div>
+		<>
+			<main className='h-min-[100%] h-screen flex flex-col'>
+				<Navbar />
+				<div className='flex-1 flex flex-col items-center justify-center p-8'>
+					<h2 className='uppercase text-4xl text-secondary font-bold tracking-wide text-center'>
+						Hola, soy <span className='text-primary'>Ian Carlos.</span> <br />{' '}
+						Desarrollador Frontend.
+					</h2>
+					<div className='flex flex-col gap-4 md:flex-row mt-[5rem]'>
+						<a className='btn'>
+							<span>Habilidades</span>
+						</a>
+						<a className='btn'>
+							<span>Proyectos</span>
+						</a>
+						<a className='btn'>
+							<span>Sobre mi</span>
+						</a>
+						<a className='btn'>
+							<span>Contacto</span>
+						</a>
+					</div>
+				</div>
+			</main>
+		</>
 	);
 }
