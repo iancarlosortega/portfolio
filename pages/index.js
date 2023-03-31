b6c349849c76d7cbabb5590bd25b7f1d53eda1b6
Ian Carlos Ortega <iancarlosortegaleon@gmail.com>
2023-03-30 22:49:39 -0500

    Remove react hook form and send emails via contact form

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,17 @@
+import Head from 'next/head';
 import { About, Contact, Footer, Main, Projects, Skills } from '../components';
 
 export default function Home() {
 	return (
 		<>
+			<Head>
+				<title>Ian Carlos Ortega | Portafolio</title>
+				<meta
+					property='og:title'
+					content='Ian Carlos Ortega | Portafolio'
+					key='title'
+				/>
+			</Head>
 			<Main />
 			<Skills />
 			<Projects />
