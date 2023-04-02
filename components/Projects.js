2b9e57a505b4fffc304af9ae089e05f5a1db15a0
Ian Carlos Ortega <iancarlosortegaleon@gmail.com>
2023-04-01 20:29:58 -0500

    Add projects descriptions and meta tags for SEO

--- components/Projects.js
+++ components/Projects.js
@@ -9,30 +9,14 @@ export const Projects = () => {
 			<div className='pb-32'>
 				<ProjectCard
 					title='FreeMove &#128692;'
-					description='Consequat labore nulla nisi sunt ullamco aliqua in anim. Esse anim et
-					velit velit incididunt Lorem quis incididunt magna tempor pariatur
-					est. Cillum aliqua mollit amet ut dolor aliqua adipisicing
-					reprehenderit reprehenderit occaecat ex irure. Ut deserunt elit dolore
-					commodo ullamco Lorem ullamco non non. Voluptate consequat Lorem
-					labore irure irure. Occaecat anim irure dolor pariatur anim id
-					incididunt dolore enim laboris dolore eiusmod cupidatat. Lorem
-					pariatur ad consectetur ex ipsum exercitation Lorem aliqua sunt velit
-					aliqua consectetur sit.'
+					description='FreeMove es un sitio web enfocado en ciclistas, donde los mismos podrán revisar las rutas que han generado mediante el aplicativo móvil disponible tanto para Android como IOs. Además, la aplicación les mostrará información más detallada a manera de gráficas como cuántas calorías han quemado, los kilómetros recorridos, entre otros.'
 					stack={['Angular', 'Firebase', 'Mapbox']}
 					demoUrl='https://freemove-aa0c5.web.app/'
 					projectImg='/projects/freemove.jpg'
 				/>
 				<ProjectCard
 					title='UTPL Play &#127909;'
-					description='Consequat labore nulla nisi sunt ullamco aliqua in anim. Esse anim et
-					velit velit incididunt Lorem quis incididunt magna tempor pariatur
-					est. Cillum aliqua mollit amet ut dolor aliqua adipisicing
-					reprehenderit reprehenderit occaecat ex irure. Ut deserunt elit dolore
-					commodo ullamco Lorem ullamco non non. Voluptate consequat Lorem
-					labore irure irure. Occaecat anim irure dolor pariatur anim id
-					incididunt dolore enim laboris dolore eiusmod cupidatat. Lorem
-					pariatur ad consectetur ex ipsum exercitation Lorem aliqua sunt velit
-					aliqua consectetur sit.'
+					description='La aplicación de UTPLPlay permite a sus usuarios encontrar un contenido educativo proveído por la Universidad Técnica Particular de Loja de una manera más amena y organizada según las distintas facultades y materias impartidas en las mismas. De esta manera, los estudiantes se podrán preparar de una mejor manera para sus clases o repasar algunos temas de su interés.'
 					stack={['Angular', 'Firebase']}
 					demoUrl='https://play.liidutpl.ec/'
 					codeUrl='https://github.com/iancarlosortega/utplPlay'
@@ -41,31 +25,15 @@ export const Projects = () => {
 				/>
 				<ProjectCard
 					title='Teslo Shop &#128085;'
-					description='Consequat labore nulla nisi sunt ullamco aliqua in anim. Esse anim et
-					velit velit incididunt Lorem quis incididunt magna tempor pariatur
-					est. Cillum aliqua mollit amet ut dolor aliqua adipisicing
-					reprehenderit reprehenderit occaecat ex irure. Ut deserunt elit dolore
-					commodo ullamco Lorem ullamco non non. Voluptate consequat Lorem
-					labore irure irure. Occaecat anim irure dolor pariatur anim id
-					incididunt dolore enim laboris dolore eiusmod cupidatat. Lorem
-					pariatur ad consectetur ex ipsum exercitation Lorem aliqua sunt velit
-					aliqua consectetur sit.'
-					stack={['Next.Js', 'React', 'MongoDB', 'Docker']}
-					demoUrl='#'
+					description='Con un diseño simple pero minimalista, Teslo busca incrementar sus ventas mediante una plataforma en línea que distribuye su ropa en 3 categorías: hombre, mujer y unisexo. Con funcionalidades necesarias como el carrito de compras y pasarela de pagos tanto para Paypal como para tarjetas de crédito.'
+					stack={['Next.Js', 'Node.Js', 'MongoDB', 'Docker']}
+					demoUrl='https://teslo-shop-topaz.vercel.app/'
 					codeUrl='https://github.com/Curso-NextJs/teslo-shop'
 					projectImg='/projects/teslo-shop.jpg'
 				/>
 				<ProjectCard
 					title='Creando Cariño &#127873;'
-					description='Consequat labore nulla nisi sunt ullamco aliqua in anim. Esse anim et
-					velit velit incididunt Lorem quis incididunt magna tempor pariatur
-					est. Cillum aliqua mollit amet ut dolor aliqua adipisicing
-					reprehenderit reprehenderit occaecat ex irure. Ut deserunt elit dolore
-					commodo ullamco Lorem ullamco non non. Voluptate consequat Lorem
-					labore irure irure. Occaecat anim irure dolor pariatur anim id
-					incididunt dolore enim laboris dolore eiusmod cupidatat. Lorem
-					pariatur ad consectetur ex ipsum exercitation Lorem aliqua sunt velit
-					aliqua consectetur sit.'
+					description='Creando Cariño es una tienda online que vende regalos personalizados según las preferencias del usuario que se puede contactar con el personal mediante WhatsApp o solicitar algún regalo que se muestre ya en el catálogo de la plataforma, la cual tiene una gran diversidad de tipos de regalos para diferentes momentos especiales.'
 					stack={['Angular', 'Firebase']}
 					demoUrl='https://creando-carino.web.app/'
 					codeUrl='https://github.com/iancarlosortega/creandoCarino'
@@ -74,8 +42,7 @@ export const Projects = () => {
 				/>
 				<ProjectCard
 					title='Arch Studio &#127961;'
-					description='Consequat labore nulla nisi sunt ullamco aliqua in anim. Esse anim et
-					velit velit incididunt..'
+					description='Arch Studio es un sitio web el cual tiene como objetivo mostrar los servicios y proyectos realizados por un arquitecto, para lograr aumentar el número de sus clientes y así poder generar más ingresos. El sitio web cuenta con un diseño minimalista y moderno, además de que se ha utilizado el framework Astro para la generación de sitios estáticos.'
 					stack={['Astro', 'React', 'TailwindCSS']}
 					demoUrl='https://arch-studio-iota.vercel.app/'
 					codeUrl='https://github.com/iancarlosortega/astro-studio'
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,51 @@
 import Head from 'next/head';
 import { About, Contact, Footer, Main, Projects, Skills } from '../components';
 
+// Generate meta tags for SEO
+
 export default function Home() {
 	return (
 		<>
 			<Head>
-				<title>Ian Carlos Ortega | Portafolio</title>
+				<title>Ian Carlos Ortega | Desarrollador Web</title>
+				<meta
+					name='description'
+					content='Desarrollador Web Full Stack, con experiencia en el desarrollo de aplicaciones web y móviles. Conocimientos en tecnologías como Angular, React, Next.Js, Node.Js, Express, MongoDB, Firebase, Docker, entre otras.'
+				/>
+				<meta
+					name='keywords'
+					content='Desarrollador Web, Desarrollador Web Full Stack, Desarrollador Web Angular, Desarrollador Web React, Desarrollador Web Next.Js, Desarrollador Web Node.Js, Desarrollador Web Express, Desarrollador Web MongoDB, Desarrollador Web Firebase'
+				/>
+				<meta name='author' content='Ian Carlos Ortega' />
+				<meta name='viewport' content='width=device-width, initial-scale=1.0' />
+				<meta httpEquiv='Content-Type' content='text/html; charset=utf-8' />
+				<meta httpEquiv='X-UA-Compatible' content='IE=edge' />
+				<meta name='robots' content='index, follow' />
+				<meta name='googlebot' content='index, follow' />
+				<meta name='google' content='nositelinkssearchbox' />
+				<meta name='google' content='notranslate' />
+				{/* Generate meta tags for SEO with open graph */}
+				<meta property='og:url' content='https://iancarlosortega.dev/' />
+				<meta property='og:type' content='website' />
+				<meta
+					property='og:image'
+					content='https://iancarlosortega.dev/images/iancarlosortega.png'
+				/>
 				<meta
 					property='og:title'
-					content='Ian Carlos Ortega | Portafolio'
+					content='Ian Carlos Ortega | Desarrollador Web'
 					key='title'
 				/>
+				<meta
+					property='og:description'
+					content='Desarrollador Web Full Stack, con experiencia en el desarrollo de aplicaciones web y móviles. Conocimientos en tecnologías como Angular, React, Next.Js, Node.Js, Express, MongoDB, Firebase, Docker, entre otras.'
+				/>
+				<meta
+					property='og:site_name'
+					content='Ian Carlos Ortega | Desarrollador Web'
+				/>
+				<meta property='og:locale' content='es_ES' />
+				<meta property='og:locale:alternate' content='en_US' />
 			</Head>
 			<Main />
 			<Skills />
