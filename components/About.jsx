c97c23ab53c163d6a3a07075240ff84319f09360
Ian Carlos Ortega <iancarlosortegaleon@gmail.com>
2023-04-18 22:01:13 -0500

    Fix cv link on about section

--- components/About.jsx
+++ components/About.jsx
@@ -5,7 +5,7 @@ import { Title } from './';
 import { DownloadIcon } from './icons';
 
 export const About = () => {
-	const { t: translate } = useTranslation('about');
+	const { t: translate } = useTranslation(['common', 'about']);
 
 	return (
 		<div className='custom-clip-shadow'>
@@ -17,7 +17,7 @@ export const About = () => {
 				<section
 					id='about'
 					className='max-w-[1110px] mx-auto px-4 md:px-12 lg:px-0 pt-24 lg:pt-48 lg:pb-48 lg:mt-24'>
-					<Title text={translate('title')} color='white' />
+					<Title text={translate('about:title')} color='white' />
 					<div className='flex flex-col md:flex-row gap-6 items-center md:items-start justify-center pb-24'>
 						<Image
 							className='md:basis-1/2 w-[250px] md:max-w-[300px] lg:max-w-[350px] md:mx-auto rounded-full drop-shadow-lg'
@@ -31,17 +31,18 @@ export const About = () => {
 								'flex md:basis-1/2 flex-col gap-4 items-center text-center text-white',
 								'text-sm lg:text-lg my-8 md:my-0 px-8 md:px-4'
 							)}>
-							<p>{translate('description.firstParagraph')}</p>
-							<p>{translate('description.secondParagraph')}</p>
+							<p>{translate('about:description.firstParagraph')}</p>
+							<p>{translate('about:description.secondParagraph')}</p>
 							<p className='mb-4'>
-								{translate('description.thirdParagraph')}&#128054;
+								{translate('about:description.thirdParagraph')}&#128054;
 							</p>
 							<a
-								href='https://drive.google.com/file/d/1RZUQ5Zct4BA9uYxNqWSipNos6uC7YDw9/view?usp=sharing'
+								className='btn btn-secondary'
+								href={translate('cv-link')}
 								target='_blank'
-								className='btn btn-secondary'>
+								rel='noreferrer noopener'>
 								<span>
-									{translate('download')} <DownloadIcon />
+									{translate('about:download')} <DownloadIcon />
 								</span>
 							</a>
 						</div>
